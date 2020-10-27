de4229835e5e7a8066c80ab6bd62c8af76c5475f
Jonah Wu <jonahjun.wu@gmail.com>
2020-10-27 08:45:07 +0800

    feature: decoupling components

---
+++ src/components/a2email.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const A2EmailValidator: React.FC = () => {
+    return (
+        <div>
+            {/* Add form and display API response */}
+	<h2>A2 Email Validation</h2>
+        </div>
+    )
+}
+
+export default A2EmailValidator
+
---
+++ src/components/email.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EmailValidator: React.FC = () => {
+    return (
+        <div>
+            {/* Add form and display API response */}
+	<h2>Email Validation</h2>
+        </div>
+    )
+}
+
+export default EmailValidator
+
