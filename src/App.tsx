08cad6d32d4b51266d55a6de7ad9a382d31cd4b5
Jonah Wu <jonahjun.wu@gmail.com>
2020-10-31 07:21:50 +0800

    feature: call function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SearchCriteria from './components/search'
+import Link from './components/search'
 
 function App() {
   return (
     <div className="App">
 	  <h1>Email Validation</h1>
       <main>
-        <SearchCriteria />
+        <Link x="3" />
       </main>
     </div>
   );
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,50 +1,33 @@
 import * as React from "react";
-import { debounce } from "lodash";
 
-interface SearchCriteriaProps {
-  placeholder: string;
-  doSearch: (criteria: string) => void;
+
+function sumf(x:number): number {
+	return x+10
+
 }
-interface SearchCriteriaState {
-  criteria: string;
-}
-
-class SearchCriteria extends React.Component<
-  SearchCriteriaProps,
-  SearchCriteriaState
-> {
-  raiseDoSearchWhenUserStoppedTyping = debounce(() => {
-    this.props.doSearch(this.state.criteria);
-    console.log(`doSearch(${this.state.criteria})`);
-  }, 300);
-
-  constructor() {
-    super();
-
-    this.state = {
-      criteria: ""
-    };
-  }
-
-  handleCriteriaChange = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({ criteria: e.currentTarget.value }, () => {
-      this.raiseDoSearchWhenUserStoppedTyping();
-    });
-  };
-
-  render() {
-    return (
-      <div className="input-group">
-        <input
-          type="text"
-          placeholder={this.props.placeholder}
-          value={this.state.criteria}
-          onChange={this.handleCriteriaChange}
-          className="form-control"
-        />
-      </div>
-    );
-  }
+const sum = (x:number): number =>{
+	return x+1
 }
 
-export default SearchCriteria;
+
+type LinkProps = {
+  x: string;
+};
+
+
+const Link: React.FC<LinkProps> = (props) => {
+  const { x, children } = props;
+	let retx:number = sum(2)
+	let retxf:number = sumf(2)
+  console.log('yes i am here ',{retx},{retxf});	
+  return (
+    <div className="link">
+      {/* ... */}
+      <h2>test {retx} {retxf}</h2>
+    </div>
+  );
+};
+
+export default Link;
+
+
