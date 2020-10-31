70051afef31726228e948fb41bb5ed0d0f1ef298
Jonah Wu <jonahjun.wu@gmail.com>
2020-10-31 09:34:38 +0800

    feature: send parameters by interface 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import Link from './components/search'
 
 function App() {
+	const ww:string="8"
   return (
     <div className="App">
 	  <h1>Email Validation</h1>
       <main>
-        <Link x="3" y="100" />
+        <Link x={ww} y="100" />
       </main>
     </div>
   );
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -15,16 +15,60 @@ type LinkProps = {
   y: string;
 };
 
+interface Props {
+   ld: string[];
+}
+
+				/*
+const DataList: React.FC<Props> = (props) => {
+function DataList(props:Props) {
+				 */
+const DataList: React.FC<Props> = (props) => {
+  const { ld, children } = props;
+  const listItems = ld.map((lds) =>
+  			<li>{lds}</li>
+ );
+  console.log('inside list ',{listItems});	
+	/*
+  const {datas, children} = props;
+  const listItems = datas.map((dd) =>
+    <li key={dd.toString()}>
+      {dd}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+	 */
+	return(
+			<div>
+							<h2>show some list </h2>
+							<ul>{listItems}</ul>
+			</div>
+	)
+}
 
 const Link: React.FC<LinkProps> = (props) => {
   const { x, y, children } = props;
-	let retx:number = sum(2)
-	let retxf:number = sumf(2)
+	let retx:number = sum(Number(x))
+	const retxf:number = sumf(2)
+ const devices: string[] = ['iphone', 'pixel', 'ipad', 'note 10'];
+ const Dev:Props={ld:['iphone', 'pixel', 'ipad', 'note 10']}
+
+ const listItems = devices.map((listData) =>
+			<li>{listData}</li>
+ );
+
   console.log('yes i am here ',{retx},{retxf});	
+
   return (
     <div className="link">
       {/* ... */}
-      <h2>test {y} {retx} {retxf}</h2>
+      <h2>test {y} {retx} {retxf} </h2>
+			<main>
+				<DataList ld={Dev.ld}/> 
+			</main>
+
     </div>
   );
 };
